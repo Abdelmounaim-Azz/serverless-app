3fc498f100cf66d5b8e93e51ff0addc463319426
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-21 23:02:36 +0100

    add middleware to lambda fnc

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,5 +1,9 @@
 import {v4 as uuid} from "uuid";
 import AWS from "aws-sdk";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function createAuction(event, context) {
   const {title} = JSON.parse(event.body);
@@ -22,4 +26,7 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler);
