934a70f1bc2ebf55d2a6033893ce595c92a8a38c
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-23 05:14:02 +0100

    add closeAuction helper

--- src/middlewares/closeAuctions.js
+++ src/middlewares/closeAuctions.js
@@ -1,10 +1,10 @@
 import AWS from "aws-sdk";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-export async function closeAuction(auctionToBeClosed) {
+export async function closeAuctions(auctionsToBeClosed) {
   const params = {
     TableName: process.env.AUCTION_TABLE_NAME,
-    key: {id: auctionToBeClosed.id},
+    key: {id: auctionsToBeClosed.id},
     UpdateExpression: "set #status = :status",
     ExpressionAttributeValues: {
       ":status": "CLOSED",
