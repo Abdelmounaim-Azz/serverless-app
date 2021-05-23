fa0882905c53b204e3e98935b68fed05e498d250
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-23 05:22:27 +0100

    close ended auctions

--- src/handlers/ticktockAuctions.js
+++ src/handlers/ticktockAuctions.js
@@ -1,7 +1,20 @@
 import {findEndedAuctions} from "../middlewares/findEndedAuctions";
+import {closeAuctions} from "../middlewares/closeAuctions";
+import createError from "http-errors";
 async function ticktockAuctions(event, context) {
-  const isAuctionClosed = await findEndedAuctions();
-  console.log(isAuctionClosed);
+  try {
+    const AuctionsToclose = await findEndedAuctions();
+    const closeEndedAuctions = AuctionsToclose.map((auction) =>
+      closeAuctions(auction.id)
+    );
+    await Promise.all(closeEndedAuctions);
+    return {
+      statusCode: 204,
+      closedAuctions: closeEndedAuctions.length,
+    };
+  } catch (error) {
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = ticktockAuctions;
