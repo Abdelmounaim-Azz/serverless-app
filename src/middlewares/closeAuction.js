6a2c1a2c8975bcd99bbfd648d73c64bd5d307d4c
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-23 05:10:46 +0100

    add closeAuction helper

---
+++ src/middlewares/closeAuction.js
@@ -0,0 +1,18 @@
+import AWS from "aws-sdk";
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export async function closeAuction(auctionToBeClosed) {
+  const params = {
+    TableName: process.env.AUCTION_TABLE_NAME,
+    key: {id: auctionToBeClosed.id},
+    UpdateExpression: "set #status = :status",
+    ExpressionAttributeValues: {
+      ":status": "CLOSED",
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+  };
+  const res = await dynamodb.update(params).promise();
+  return res;
+}
