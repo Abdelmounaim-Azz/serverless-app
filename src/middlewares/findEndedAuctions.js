73eef330f46a42254777771fa9e80a872b20ca49
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-23 04:53:12 +0100

    tablename src of issue fix

--- src/middlewares/findEndedAuctions.js
+++ src/middlewares/findEndedAuctions.js
@@ -4,7 +4,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 export async function findEndedAuctions() {
   const now = new Date();
   const params = {
-    TableName: process.env.AUCTIONS_TABLE_NAME,
+    TableName: process.env.AUCTION_TABLE_NAME,
     IndexName: "statusAndEndDate",
     KeyConditionExpression: "#status = :status AND endAt <= :now",
     ExpressionAttributeValues: {
