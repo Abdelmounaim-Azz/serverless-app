e9e425a00f22c57ef0cef1c8608881de9a16f8b1
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-23 15:04:27 +0100

    query based on status

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -4,10 +4,23 @@ import createError from "http-errors";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function getAuctions(event, context) {
   let auctions;
+  const {status} = event.queryStringParameters;
+  let auctions;
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: "statusAndEndDate",
+    KeyConditionExpression: "#status = :status",
+    ExpressionAttributeValues: {
+      ":status": status,
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+  };
+
   try {
-    const res = await dynamodb
-      .scan({TableName: process.env.AUCTION_TABLE_NAME})
-      .promise();
+    const res = await dynamodb.query(params).promise();
     auctions = res.Items;
   } catch (error) {
     throw new createError.InternalServerError(error);
