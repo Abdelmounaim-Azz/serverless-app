ef6b044d8831986f0e66de7c7fcf3f26f9eb23ef
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-24 05:54:13 +0100

    motherfucking validator version issues fixed

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,11 +1,26 @@
 import AWS from "aws-sdk";
+const validator = require("@middy/validator");
 import {cmnMiddleware} from "../middlewares/middy";
 import createError from "http-errors";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const getAuctionsSchema = {
+  required: ["queryStringParameters"],
+  properties: {
+    queryStringParameters: {
+      type: "object",
+      properties: {
+        status: {
+          type: "string",
+          enum: ["OPEN", "CLOSED"],
+          default: "OPEN",
+        },
+      },
+    },
+  },
+};
 async function getAuctions(event, context) {
   let auctions;
   const {status} = event.queryStringParameters;
-
   const params = {
     TableName: process.env.AUCTION_TABLE_NAME,
     IndexName: "statusAndEndDate",
@@ -31,4 +46,6 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = cmnMiddleware(getAuctions);
+export const handler = cmnMiddleware(getAuctions).use(
+  validator({inputSchema: getAuctionsSchema, useDefaults: true})
+);
