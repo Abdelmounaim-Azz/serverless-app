a72d77b7750c7d0faf012d55ab709725cd5a30de
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-26 08:29:18 +0100

    notify both bidder and seller when time passes and auction closed.

--- src/middlewares/closeAuctions.js
+++ src/middlewares/closeAuctions.js
@@ -1,6 +1,6 @@
 import AWS from "aws-sdk";
-
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 export async function closeAuctions(auctionToBeClosed) {
   const params = {
@@ -15,7 +15,28 @@ export async function closeAuctions(auctionToBeClosed) {
     },
   };
 
-  const res = await dynamodb.update(params).promise();
-
-  return res;
+  await dynamodb.update(params).promise();
+  const {title, seller, highestBid} = auctionToBeClosed;
+  const {amount, bidder} = highestBid;
+  const sendMsgToSeller = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: "Auction has been closed and Item has been sold.",
+        recipient: seller,
+        body: `Congratulations!Your item ${title} has been sold for $${amount}`,
+      }),
+    })
+    .promise();
+  const sendMsgToBidder = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: "Hooray!you just won an auction.",
+        recipient: bidder,
+        body: `Congratulations on winning an auction of ${title} for a $${amount}.`,
+      }),
+    })
+    .promise();
+  return Promise.all([sendMsgToSeller, sendMsgToBidder]);
 }
